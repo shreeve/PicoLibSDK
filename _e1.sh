e5538bf1299bd9539025cfa090e4701d05bf7c2d
Steve Shreeve <steve.shreeve@gmail.com>
2023-12-20 19:30:04 -0700

    On macOS, ignore file xattrs and use the correct file path

--- _e1.sh
+++ _e1.sh
@@ -5,7 +5,10 @@
 #   or change disk name of the Pico image in task manager.
 
 if [[ -e "${TARGET}".uf2 ]]; then
-  cp "${TARGET}".uf2 /media/        # Change this name of the mount point folder of your RPI-RP2 here.
+  if [[ -e /Volumes/RPI-RP2/ ]]; then
+    # cp "${TARGET}".uf2 /media/        # Change this name of the mount point folder of your RPI-RP2 here.
+    cp -x "${TARGET}".uf2 /Volumes/RPI-RP2/ # Change this name of the mount point folder of your RPI-RP2 here.
+  fi
 fi
 
 
